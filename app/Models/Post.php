49f645dae19ad9d80f442d318e7218c527e23a8a
Anugrah Prasetya <anugrah0211@gmail.com>
2025-05-16 18:25:06 +0700

    UAS

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    protected $fillable = ['title', 'slug', 'excerpt', 'content', 
+    protected $fillable = ['title', 'slug', 'categories', 'tags', 'excerpt', 'content', 
                             'published', 'published_at', 'user_id'];
 
     public function categories()
     {
         return $this->belongsToMany(Category::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tags::class);
     }
 }
---
+++ app/Models/Tags.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tags extends Model
+{
+        //
+    protected $fillable = ['name', 'slug'];
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tags::class);
+    }
+}
