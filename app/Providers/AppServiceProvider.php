b77b1263b76e5756bfd7b0a696c645371cac3c0e
Nunu <Nunu@gmail.com>
2025-07-04 21:14:59 +0700

    Update 9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::Policy(Post::class, PostPolicy::class);
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS', 'on');
+            URL::forceScheme('https');
+        }
     }
 }
