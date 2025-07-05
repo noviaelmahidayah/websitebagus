e3e7f166e6fcb6938fee7a153b9d87a77654411e
Nunu <Nunu@gmail.com>
2025-07-05 18:35:52 +0700

    Update Prod

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -34,7 +34,7 @@
         <a href="{{url('penulis')}}" class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Penulis
         </a>
         </nav>
-        <a href="http://localhost:8000/admin/login" class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
+        <a href="https://praktekfilabelanugrah-production.up.railway.app/admin/login" class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
