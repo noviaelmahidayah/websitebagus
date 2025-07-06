9594c0752e1663d34ee1f17b441e66a54ac401c4
Elma Hidayah <elmahidayah05@gmail.com>
2025-07-06 20:36:42 +0700

    baru5

--- resources/views/home/articles.blade.php
+++ resources/views/home/articles.blade.php
@@ -7,7 +7,7 @@
       <a href = "{{ route('post.detail', ['slug'=> $featured_post->slug]) }}"><img class="object-cover object-center rounded" src="{{$featured_post->getFirstMediaUrl('featured_image')}}" /></a>
     </div>
     <div id ="blog-content" class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <a href = "{{ route('post.detail', ['slug'=> $featured_post->slug]) }}"><h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">{{$post->title}}</h1></a>
+      <a href = "{{ route('post.detail', ['slug'=> $featured_post->slug]) }}"><h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">{{$post->title}}</h1></a>
       <a href = "{{ route('post.detail', ['slug'=> $featured_post->slug]) }}"><p class="mb-8 leading-relaxed">{{$post->excerpt}}</p></a>
       <div class="flex justify-center">
         <a href = "{{ route('post.detail', ['slug' => $post->slug]) }}" class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">Lihat Selengkapnya</a>
